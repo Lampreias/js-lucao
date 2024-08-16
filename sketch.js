16ac99e15c5bca732b9c67910224163ba4e03713
Lampreias <gu.rod1906@gmail.com>
2024-08-16 09:47:21 -0300

    adicionando aequivos js e css

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(600, 600);
+  background("yellow");
+  }
+    function draw() {
+    
+  
+    stroke("orange");
+    fill("blue");
+  
+    //console.log(mouseIsPressed)
+    if (mouseIsPressed) 
+      rect(mouseX, mouseY, 30, 30
+          )
+    }
